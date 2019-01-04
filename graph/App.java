25bdcee46dd7793da330c50220f5aa4cec72ac2d
sebrown0 <thebrownfamily@live.co.uk>
2019-01-04 14:05:36 +0100

    Completed basic Graph structure

--- graph/App.java
+++ graph/App.java
@@ -5,7 +5,23 @@ public class App {
 	public static void main(String[] args) {
 
 		Graph graph = new Graph(5);
-
+		
+		graph.addVertex("State St");
+		graph.addVertex("Avenel St");
+		graph.addVertex("Pocono St");
+		graph.addVertex("William Ave");
+		graph.addVertex("Elm St");
+		
+		graph.addEdge("State St", "Avenel St");
+		graph.addEdge("State St", "Elm St");
+		graph.addEdge("Avenel St", "Pocono St");
+		graph.addEdge("Pocono St", "Elm St");
+		graph.addEdge("William Ave", "Pocono St");
+		graph.addEdge("William Ave", "State St");
+		graph.addEdge("Elm St", "Avenel St");
+		
+		graph.displayVerticesWithEdges();
+		
 	}
 
 }
--- graph/Graph.java
+++ graph/Graph.java
@@ -1,43 +1,85 @@
 package ds.graph;
 
-import java.util.ArrayList;
-
 import ds.linked_list.LinkedList;
+import ds.linked_list.Node;
 
 public class Graph {
-	private int vCount; // Number of vertices
-	private int eCount; // Number of edges
-	private ArrayList<Integer>[] adjacents; // Array of integer lists
-	private LinkedList[] arrayList;
+	private int maxVertices; // Max number of vertices.
+	private int vertexCount; // Number of vertices.
+	private int[] eCount; // Number of edges for each vertex.
 
-	public Graph(int vCount) {
+	private LinkedList[] vertexList; // Array of vertices (Vertex is first node, Edges follow).
+
+	public Graph(int maxVertices) {
 		super();
-		this.vCount = vCount;
-		this.eCount = 0;
-		adjacents = new ArrayList[vCount];
-		arrayList = new LinkedList[vCount];
+		this.maxVertices = maxVertices;
+		this.vertexCount = 0;
+		this.eCount = new int[maxVertices];
+		vertexList = new LinkedList[maxVertices];
 
-		// Create new empty list for each vertex
-		for (int i = 0; i < vCount; i++) {
-			adjacents[i] = new ArrayList<Integer>();
-			arrayList[i] = new LinkedList();
+	}
+
+	// Add a new vertex to the list.
+	// If it doesn't already exist or max out the array.
+	public void addVertex(String vertex) {
+		boolean foundVertex = false;
+
+		if (vertexCount < maxVertices) {
+			for (int i = 0; i < vertexCount; i++) {
+				if (vertexList[i].getFirstNode().getValue().equals(vertex)) {
+					// Already have that vertex
+					foundVertex = true;
+					break;
+				}
+			}
+
+			// Vertex doesn't exist so add it to the vertex list.
+			if (!foundVertex) {
+				vertexList[vertexCount] = new LinkedList();
+				vertexList[vertexCount].insertFirst(vertex);
+				vertexCount++;
+			}
+		} else {
+			System.out.println("Reached limit. Can't add anymore Vertices");
 		}
 	}
 
-	public int getVertexCount() {
-		return vCount;
+	// If the source Vertex exists add edge to next node in list.
+	public String addEdge(String src, String dest) {
+		String msg = "Vertex: " + src + " doesn't exist!";
+
+		for (int i = 0; i < vertexCount; i++) {
+			if (vertexList[i].getFirstNode().getValue().equals(src)) {
+				// Found the vertex add the edge.
+				vertexList[i].insertLast(dest);
+				// Increment the number of edges for this vertex.
+				eCount[i]++;
+				msg = "Successfully inserted edge: " + dest + " into: " + src;
+				break;
+			}
+		}
+
+		return msg;
 	}
 
-	public int getEdgeCount() {
-		return eCount;
+	// Display list of vertices.
+	public void displayVertices() {
+		for (int i = 0; i < vertexCount; i++) {
+			System.out.println(vertexList[i].getFirstNode().getValue());
+		}
 	}
 
-	public void addEdge(int src, int dest) {
-		adjacents[src].add(dest);
-		eCount++;
+	// Display list of vertices with their edges.
+	public void displayVerticesWithEdges() {
+		System.out.println("\nVertices and their Edges ->");
+		for (int i = 0; i < vertexCount; i++) {
+			Node<Object> v = vertexList[i].getFirstNode();
+			System.out.println("\nVertex -> " + v.getValue() + "");
+			while (v.getNextNode() != null) {
+				v = v.getNextNode();
+				System.out.print("\t--> " + v.getValue());
+			}
+		}
 	}
 
-	public Object[] adj(int src) {
-		return adjacents[src].toArray();
-	}
 }
